47f1aa9f172376a4f4b6c8d2f9e3120039f2cccb
adityash1 <aditya167411@gmail.com>
2022-11-25 17:53:40 +0530

    chat history component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { connect, sendMsg } from "./api";
 import Header from "./components/Header";
+import ChatHistory from "./components/ChatHistory/ChatHistory";
 
 const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+
   useEffect(() => {
-    connect();
-  }, []);
+    connect((msg) => {
+      console.log("New Message");
+      setChatHistory((chatHistory) => [...chatHistory, msg]);
+      console.log(chatHistory);
+    });
+  }, [chatHistory]);
 
   const send = () => {
     console.log("send");
@@ -16,6 +23,7 @@ const App = () => {
   return (
     <div className="App">
       <Header />
+      <ChatHistory chatHistory={chatHistory} />
       <button onClick={send}>Hit</button>
     </div>
   );
